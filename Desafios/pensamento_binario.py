15a093cd8653bcbbb0aed4f714a82857a24faa28
agathaateixeira.academic@gmail.com <agathaateixeira.academic@gmail.com>
2024-04-07 19:39:49 -0300

    Aula 2

---
+++ Desafios/pensamento_binario.py
@@ -0,0 +1,44 @@
+
+# Operadores Aritméticos
+# "+" equivale a adição
+# "-" equivale a subtração
+# "*" equivale a multiplicação
+# "/" equivale a divisão
+# "**" equivale a exponenciação
+# "//" equivale a parte inteira
+# "%" equivale ao módulo
+
+# Operadores Relacionais
+# > Maior do que
+# >= Maior ou igual a
+# < Menor do que
+# <= Menor ou igual a
+# == Igual a 
+# != Diferente de
+
+a = 4
+b = 2
+c = 5
+
+if ((a ** b) + a) != (a * c):
+    print("A expressão 1 é verdade")
+else:
+    print("A expressão 1 é verdade") 
+
+
+if ((a * c) > (a + b)):
+   print("A expressão 2 é verdade")
+else:
+    print("A expressão 2 é verdade")
+
+
+if ((a+ b + c) == 11):
+     print("A expressão 3 é verdade")
+else:
+    print("A expressão 3 é verdade")
+
+
+if (( c // b ) > (a / b)):
+    print("A expressão 4 é verdadeira")
+else:
+    print("A expressão 4 é falsa")
---
+++ Desafios/vetores.py
@@ -0,0 +1,6 @@
+#val = ('val0','val1','val2'...)
+val = tuple(('val0','val1','val2')) #Pq possui 2 parenteses???
+
+#retorna o número de vezes que o elemento aparece na tupla
+val.count('Valor')
+val.count('val0')
